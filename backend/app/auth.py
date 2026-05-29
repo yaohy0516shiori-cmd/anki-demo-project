ce8db558e65661af7e54ea3976209d08792d30b0
shiori <yaohy0516shiori@gmail.com>
2026-05-29 15:42:47 +0800

    fix(auth.py):all env var should read from settings.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,25 +1,23 @@
 from datetime import datetime, timedelta,timezone
 import jwt
 from fastapi import HTTPException
-import os
-
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me-minimum-32-bytes-long") # if not set, use a default value
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
+from backend.app.settings import get_settings,Settings
 
 # function to create access token, used to authenticate user
 def create_access_token(user_id: int):
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    settings=get_settings()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
     payload = {
         "sub": str(user_id),
         "exp": expire,
     }
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
 
 # function to decode access token, used to get user id from token
 def decode_access_token(token: str) -> int:
+    settings=get_settings()
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
         user_id = int(payload["sub"])
     except Exception:
         raise HTTPException(status_code=401, detail="Invalid token")
